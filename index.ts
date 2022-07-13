bcb181c6182266ab385fe99df7429873b7f3417a
Myrthe Rijk <mjrijk@hotmail.com>
2022-07-13 23:13:39 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,48 @@
+import courses from './courses';
+import studyGroups from './studyGroups';
+
+type Course = {
+  id: number;
+  studyGroupId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type StudyGroup = {
+  id: number;
+  courseId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type SearchEventsOptions = {
+  query: string | number;
+  eventType: 'courses' | 'groups';
+}
+
+let enrolledEvents: (Course | StudyGroup)[] = [];
+
+function searchEvents(options: SearchEventsOptions) {
+  const events: (Course | StudyGroup)[] = options.eventType === 'courses' ? courses : studyGroups;
+
+  return events.filter((event: Course | StudyGroup) => {
+    if (typeof options.query === 'number') {
+      return options.query === event.id;
+    }
+    if (typeof options.query === 'string') {
+      return event.keywords.includes(options.query);
+    }
+  })
+}
+
+function enroll(event: Course | StudyGroup) {
+  enrolledEvents = [...enrolledEvents, event];
+}
+
+const searchResults = searchEvents({ query: 'art', eventType: 'courses' })
+
+enroll(searchResults[0]);
+
+console.log(enrolledEvents);
